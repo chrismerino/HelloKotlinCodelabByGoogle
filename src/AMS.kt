111f6d1a3692df36518e518dc225ca9c0542c3e8
Chris Merino <10505127+chrismerino@users.noreply.github.com>
2020-01-23 09:36:45 -0600

    My first commit :D

---
+++ src/AMS.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}!")
+    feedTheFish()
+}
+
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
